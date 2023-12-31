2094213be00ec4ecc8a96490b794bb8bc17962a0
DLew <96327749+DLew2024@users.noreply.github.com>
2023-12-31 04:01:25 -0600

    Save1

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,10 +1,14 @@
-
+import Student from "./Student";
 
 function App() {
   
     return(
       <>
-       
+          <Student name="DLew" age={30} isStudent={1}/>
+          <Student name="Milani" age={32} isStudent={true}/> 
+          <Student name="Lailani" age={29} isStudent={false}/>
+          <Student name="CHIC" age={11} isStudent={false}/>
+          <Student name="FIC" age={19} isStudent={true}/>
       </>
       
     );
