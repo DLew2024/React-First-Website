0593e34e70a2fa6e1b050abde32a8f0e3934c9d3
DLew <96327749+DLew2024@users.noreply.github.com>
2023-12-31 00:14:53 -0600

    First react example following Bro Code on Youtube

---
+++ my-react-app/src/Footer.jsx
@@ -0,0 +1,15 @@
+
+function Footer(){
+
+    return(
+
+        <footer>
+            <p>
+                &copy; DLew first website..
+            </p>
+        </footer>
+
+    );
+}
+
+export default Footer
