7bd10c9228cca0b0accfd55529473541f8a359b9
DLew <96327749+DLew2024@users.noreply.github.com>
2023-12-31 19:59:39 -0600

    base

--- my-react-app/src/MyComponent.jsx
+++ my-react-app/src/MyComponent.jsx
@@ -1,37 +1,11 @@
 
-import React, {useState} from "react"
+
 
 
 function MyComponent(){
 
-    const [name, setName] = useState("Guest");
-    const [age, setAge] = useState(0);
-    const [isEmployed, setisEmployed] = useState(false);
-
-
-
-    const updateName = () => {
-       setName("DLew");
-    }
-
-    const incrementAge = () => {
-        setAge(age + 1)
-    }
-
-     const toggleEmployed = () => {
-        setisEmployed(!isEmployed)
-    }
-
-    return( <div>
-                <p>Name: {name}</p>
-                <button onClick={updateName}>Set Name</button>
-
-                <p>Age: {age}</p>
-                <button onClick={incrementAge}>Update Age</button>
-
-                <p>Is employed: {isEmployed ? "Yes" : "No"}</p>
-                <button onClick={toggleEmployed}>Is Employed</button>
-            </div>
+   
+    return( <></>
 
     )
 
